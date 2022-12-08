b9b615b3657ff1560661b940a426f0760813b7ed
AgendiaF <119266843+AgendiaF@users.noreply.github.com>
2022-12-08 17:47:59 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function setup()
+{
+    canvas = createCanvas(1000, 1000);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(ClassifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function clearCanvas()
+{
+    background("white")
+}
+
+
+
+function draw(ez)
+{
+    strokeWeight(10);
+    stroke(40);
+    if (mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function ClassifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label:' + results[0].label;
+
+    document.getElementById('confidence').innerHTML = 'Confidence:' + Math.round(results[0].confidence*100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
